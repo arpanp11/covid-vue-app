b7e87b31eca23129b995e9dfdf8d466908b7507e
arpanp11 <arpanpatel2810@gmail.com>
2021-07-27 16:07:31 -0400

    add country data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,15 @@
 <template>
   <main v-if="!loading">
     <DataTitle :text="title" :dataDate="dataDate" />
+    <DataBoxes :stats="stats" />
+    <CountrySelect @get-country="getCountryData" :countries="countries" />
+    <button
+      v-if="stats.Country"
+      @click="clearCountryData"
+      class="bg-green-700 text-white rounded p-3 mt-10 focus:outline-none hover:bg-green-600"
+    >
+      Reset
+    </button>
   </main>
 
   <main class="flex flex-col align-center justify-center text-center" v-else>
@@ -13,10 +22,15 @@
 
 <script>
 import DataTitle from '@/components/DataTitle';
+import DataBoxes from '@/components/DataBoxes';
+import CountrySelect from '@/components/CountrySelect';
+
 export default {
   name: 'Home',
   components: {
     DataTitle,
+    DataBoxes,
+    CountrySelect,
   },
   data() {
     return {
@@ -33,6 +47,17 @@ export default {
       const res = await fetch('https://api.covid19api.com/summary');
       const data = await res.json();
       return data;
+    },
+    getCountryData(country) {
+      this.stats = country;
+      this.title = country.Country;
+    },
+    async clearCountryData() {
+      this.loading = true;
+      const data = await this.fetchCovidData();
+      this.title = 'Global';
+      this.stats = data.Global;
+      this.loading = false;
     },
   },
   async created() {
